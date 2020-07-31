904c6477adc631f8ad597b919e59895335c420a6
Sfue <50198413+abh80@users.noreply.github.com>
2020-07-31 10:03:34 +0530

    Add files via upload

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+import fillWithEmoji from './function';
+import Canvas from 'canvas';
+declare const _default: {
+    fillWithEmoji: (ctx: any, text: any, x: any, y: any) => Promise<void>;
+    Canvas: typeof Canvas;
+};
+export default _default;
+export { fillWithEmoji, Canvas };
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import fillWithEmoji from "./function";
+import Canvas from "canvas";
+export default { fillWithEmoji, Canvas };
+export { fillWithEmoji, Canvas };
